a8add26f76a4c3f06e79406748c90f68d69ae744
Michal Kononenko <michalkononenko@gmail.com>
2025-12-19 13:46:41 -0500

    Wrote some templating

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,24 @@ from app import app
 
 user = {"username": "Michal"}
 
+posts = [
+  {
+      'author': {'username': 'Foo'},
+      'body': 'Welcome!'
+  },
+  {
+      'author': {'username': 'Bar'},
+      'body': 'Good to be here!'
+  }
+]
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home', user=user)
+    return render_template(
+      'index.html', 
+      title='Home', 
+      user=user, 
+      posts=posts
+    )
 
