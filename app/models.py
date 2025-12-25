07944e84c53fd9e38ed20f6ace0659532aa362cf
Michal Kononenko <michalkononenko@gmail.com>
2025-12-24 21:09:12 -0500

    Added Post model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -8,11 +9,31 @@ class User(db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    posts : so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author'
+    )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '{0}(id={1},username={2})'.format(
             self.__class__.__name__, 
             self.id,
             self.username
+        )
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(280))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+      index=True, default=lambda: datetime.now(timezone.utc)
+    )
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    author : so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self) -> str:
+        return '{0}(id={1},timestamp={2},user_id={3})'.format(
+            self.__class__.__name__,
+            self.id,
+            self.timestamp.isoformat(),
+            self.user_id
         )
 
